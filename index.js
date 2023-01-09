06269ce84287c520f79df5a1f0348e489aaefaf5
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-10 03:16:43 +0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-var randomNumber1 = Math.floor(Math.random()*7);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImageLeft = "dice" + randomNumber1 + ".png";
+var randomDiceImageRight = "dice" + randomNumber2 + ".png";
+
+var randomImageSource1 = "images/" + randomDiceImageLeft;
+var randomImageSource2 = "images/" + randomDiceImageRight;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
