e8e75147f23bb467050a6aa7c9500ffb603ad8e0
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-10 03:41:30 +0600

    modified

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var randomImageSource2 = "images/" + randomDiceImageRight;
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
+
+
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
